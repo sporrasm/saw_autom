078d5221e7e67103859aeb644dd4bb72ba0f090f
Santeri Porrasmaa <santeri.porrasmaa@aalto.fi>
2021-03-07 11:04:52 +0200

    Remove interrupt, add blinker relay

--- src/saw_autom/saw_autom.ino
+++ src/saw_autom/saw_autom.ino
@@ -6,41 +6,55 @@ Created on 28.02.2021
 Initially written by Santeri Porrasmaa
 */
 
+// Pins:
 byte toggleAutomPin=2; 
+byte blinker=3;
 byte sawRelay=4; 
 byte splitRelay=5; 
 byte sawSensor=6; 
 byte splitSensor=7; 
+//  boolean flag for toggling automation based on user input
 bool runLoop=false;
+
+unsigned long debounceTime = 1000; 
+unsigned long lastBounce = 0;
+unsigned long looptime = 0;
 
 void setup() {
     pinMode(sawRelay, OUTPUT);
     pinMode(splitRelay, OUTPUT);
+    pinMode(blinker, OUTPUT);
     pinMode(sawSensor, INPUT);
     pinMode(splitSensor, INPUT);
-    attachInterrupt(digitalPinToInterrupt(toggleAutomPin), toggleAutom, RISING);
-    // Relays are active low
+    pinMode(toggleAutomPin, INPUT);
+    // Relays are active low, turn off initially
     digitalWrite(sawRelay, 1);
     digitalWrite(splitRelay, 1);
-}
-
-void toggleAutom() {
-    /*
-    ISR TO TOGGLE ON OR OFF THE AUTOMATION LOOP
-    */
-    if (runLoop) {
-        runLoop=false;
-    }
-    else {
-        runLoop=true;
-    }
+    digitalWrite(blinker, 1);
 }
 
 void loop() {
-    if (runLoop) {
+    if (!(digitalRead(toggleAutomPin))) {
+        lastBounce = millis(); // Initial press registered
+        while (!(digitalRead(toggleAutomPin))) { // Loop until button released 
+            looptime = millis(); 
+        }
+        if (looptime - lastBounce >= debounceTime) { // Perform action if button was pressed for 1s or more
+            if (runLoop) { // Turn off automation & blinker
+                runLoop=false;
+                digitalWrite(blinker, 0);
+            }
+            else { // Turn on automation and blinker
+                runLoop=true;
+                digitalWrite(blinker, 1);
+            }
+        }
+    }
+    else {
+        if (runLoop) {
         delay(1000); // Wait 1s
         digitalWrite(sawRelay, 0); // Turn on saw
-        while (!(digitalRead(sawSensor))) {} // Loop until log has been saw blade is at extreme position 
+        while (!(digitalRead(sawSensor))) {} // Loop until saw blade is at extreme position 
         delay(200); // Wait 0.2s to ensure log has been sawed
         digitalWrite(sawRelay, 1); // Turn off saw
         delay(1000); // Ensure wood is in through
@@ -48,5 +62,6 @@ void loop() {
         delay(1000); 
         digitalWrite(splitRelay, 1); // Turn off splitting
         while(!(digitalRead(splitSensor))) {} // Loop until log is split
+        }
     }
 } 
